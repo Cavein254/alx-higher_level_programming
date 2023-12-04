d0aa1a678d3c63143bc101816fe67044f8499eb0
johnmuthua <caveomuthama@gmail.com>
2023-12-04 22:14:18 +0300

    Add more

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -11,5 +11,5 @@ def print_matrix_integer(matrix=[[]]):
     """
     for r in matrix:
         for c in r:
-            print(c, end=' ')
-        print('$')
+            print("{:d}".format(c), end=" " if c != r[-1] else "")
+        print()
