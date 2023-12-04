87ee12edf2880d056f74c166bd3caaf0a8b4c095
johnmuthua <caveomuthama@gmail.com>
2023-12-04 21:21:46 +0300

    Add reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """
+    This function prints numbers on reveses a list line give a list
+
+    Parameters:
+    - []: A list of numbers
+
+    Returns:
+    List of numbers in reverse
+    """
+    my_list.reverse()
+    for x in my_list:
+        print("{:d}".format(x))
