f08d3929ba5a32cb6e57224ac85c738c8c8349d0
johnmuthua <caveomuthama@gmail.com>
2023-12-04 19:25:16 +0300

    Add gitignore

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    This function prints numbers on 
+    single line give a list
+
+    Parameters:
+    - []: A list of numbers
+
+    Returns:
+    List of numbers
+    """
+
+    for x in my_list:
+        print("{}".format(x))
