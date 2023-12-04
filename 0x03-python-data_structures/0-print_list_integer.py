b04e4a776e354f58bad70c022e9fd5f994e8680a
johnmuthua <caveomuthama@gmail.com>
2023-12-04 19:39:59 +0300

    chore: Run linter

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,7 @@
 #!usr/bin/python3
 def print_list_integer(my_list=[]):
     """
-    This function prints numbers on 
-    single line give a list
+    This function prints numbers on single line give a list
 
     Parameters:
     - []: A list of numbers
