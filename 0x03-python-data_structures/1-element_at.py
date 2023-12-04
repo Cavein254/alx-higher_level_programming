6f87de71bc84d69030d5c699a94705096917e2e5
johnmuthua <caveomuthama@gmail.com>
2023-12-04 20:06:21 +0300

    Add 01

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    This function prints list like c
+
+    Parameters:
+    - my_list: A list of numbers
+    - idx: index
+
+    Returns:
+    List of numbers
+    """
+    if (idx < 0):
+        return None
+    if (idx > len(my_list)):
+        return None
+    return my_list[idx]
