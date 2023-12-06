da2f8dd99112799f5abc85f7ab8644155a8d66d0
johnmuthua <caveomuthama@gmail.com>
2023-12-06 11:30:52 +0300

    chore: more more

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-counter = 1
-num_of_args = len(sys.argv) - 1
-if (num_of_args < 1):
-    print("{} arguments.".format(num_of_args))
-elif (num_of_args == 1):
-    print("{} argument:".format(num_of_args))
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(num_of_args))
-    while ((num_of_args > 2) and (counter != num_of_args + 1)):
-        print("{}: {}".format(counter, sys.argv[counter]))
-        counter = counter + 1
+if __name__ == "__main__":
+    import sys
+    counter = 1
+    num_of_args = len(sys.argv) - 1
+    if (num_of_args < 1):
+        print("{} arguments.".format(num_of_args))
+    elif (num_of_args == 1):
+        print("{} argument:".format(num_of_args))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(num_of_args))
+        while ((num_of_args > 2) and (counter != num_of_args + 1)):
+            print("{}: {}".format(counter, sys.argv[counter]))
+            counter = counter + 1
