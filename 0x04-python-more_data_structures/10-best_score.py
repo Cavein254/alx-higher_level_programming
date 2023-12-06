ef927ee08502c19ab35108bad6acd2e1a58abd34
johnmuthua <caveomuthama@gmail.com>
2023-12-06 22:56:36 +0300

    Add 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,19 @@
+def best_score(a_dictionary):
+    """
+    A function key with biggest value
+    
+    Parameters:
+    - a_dictionary: A given dict
+
+    Returns:
+    biggest key
+    """
+    if a_dictionary:
+        my_list = list(a_dictionary.keys())
+        score = 0
+        leader = ""
+        for i in my_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                leader = i
+        return leader
