70c102a7e718bb713deec21854d62c40e286b190
johnmuthua <caveomuthama@gmail.com>
2023-12-06 11:29:16 +0300

    chore: more typos

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ import sys
 counter = 1
 num_of_args = len(sys.argv) - 1
 if (num_of_args < 1):
-    print("0 arguments.")
+    print("{} arguments.".format(num_of_args))
 elif (num_of_args == 1):
-    print("1 argument:")
+    print("{} argument:".format(num_of_args))
     print("1: {}".format(sys.argv[1]))
 else:
-    print("{} arguments".format(num_of_args))
+    print("{} arguments:".format(num_of_args))
     while ((num_of_args > 2) and (counter != num_of_args + 1)):
         print("{}: {}".format(counter, sys.argv[counter]))
         counter = counter + 1
